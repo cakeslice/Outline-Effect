293f9659a63d36d835b3332e01afe65da5ffe5e2
José Guerreiro <jose-guerreiro@live.com.pt>
2015-03-26 19:24:26 +0000

    Added license to files

--- OutlineEffect/OutlineEffect.cs
+++ OutlineEffect/OutlineEffect.cs
@@ -1,3 +1,27 @@
+/*
+//  Copyright (c) 2015 José Guerreiro. All rights reserved.
+//
+//  MIT license, see http://www.opensource.org/licenses/mit-license.php
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+*/
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -84,7 +108,7 @@ public class OutlineEffect : MonoBehaviour
             {
                 if (outlineRenderers[i] != null)
                 {
-                    originalMaterials[i] = outlineRenderers[i].material;
+                    originalMaterials[i] = outlineRenderers[i].sharedMaterial;
                     originalLayers[i] = outlineRenderers[i].gameObject.layer;
 
                     outlineRenderers[i].sharedMaterial = outlineSourceMaterial;
@@ -100,7 +124,7 @@ public class OutlineEffect : MonoBehaviour
             {
                 if (eraseRenderers[i] != null)
                 {
-                    originalEraseMaterials[i] = eraseRenderers[i].material;
+                    originalEraseMaterials[i] = eraseRenderers[i].sharedMaterial;
                     originalEraseLayers[i] = eraseRenderers[i].gameObject.layer;
 
                     eraseRenderers[i].sharedMaterial = outlineEraseMaterial;
