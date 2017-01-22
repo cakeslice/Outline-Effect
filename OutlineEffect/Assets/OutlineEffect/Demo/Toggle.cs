de7c65367e8b01a264504949a675ad9f43ff2e54
José Guerreiro <jose-guerreiro@live.com.pt>
2017-01-22 19:24:16 +0000

    Added outline toggle example to Demo

---
+++ OutlineEffect/Assets/OutlineEffect/Demo/Toggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            GetComponent<Outline>().enabled = !GetComponent<Outline>().enabled;
+        }
+	}
+}
