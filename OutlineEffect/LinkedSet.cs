39c97c94ff6c940b0d2857dcc2e23cce75587aa9
Jimmy Cushnie <jimmycushnie@gmail.com>
2019-02-17 01:41:54 -0500

    cleaned up the weirdness in LinkedSet

--- OutlineEffect/LinkedSet.cs
+++ OutlineEffect/LinkedSet.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 
 namespace cakeslice
 {
+    /// <summary>
+    /// A collection with the fast iteration time of a List and the no-duplicates and fast Remove/Contains time of a HashSet.
+    /// </summary>
 	public class LinkedSet<T> : IEnumerable<T>
 	{
-
 		private LinkedList<T> list;
-
 		private Dictionary<T, LinkedListNode<T>> dictionary;
 
 		public LinkedSet()
@@ -21,112 +22,52 @@ namespace cakeslice
 			list = new LinkedList<T>();
 			dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
 		}
-
-		public bool Contains(T t)
-		{
-			return dictionary.ContainsKey(t);
-		}
-
+		
+        /// <summary>
+        /// returns true if the item did not already exist in the LinkedSet
+        /// </summary>
 		public bool Add(T t)
 		{
-
 			if (dictionary.ContainsKey(t))
-			{
 				return false;
-			}
 
 			LinkedListNode<T> node = list.AddLast(t);
 			dictionary.Add(t, node);
 			return true;
-
 		}
 
-		public void Clear()
+        /// <summary>
+        /// returns true if the item did previously exist in the LinkedSet
+        /// </summary>
+        public bool Remove(T t)
+        {
+            LinkedListNode<T> node;
+
+            if (dictionary.TryGetValue(t, out node) && dictionary.Remove(t))
+            {
+                list.Remove(node);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Clear()
 		{
 			list.Clear();
 			dictionary.Clear();
 		}
 
-		public AddType AddOrMoveToEnd(T t)
-		{
+        public bool Contains(T t)
+            => dictionary.ContainsKey(t);
 
-			LinkedListNode<T> node;
+        public IEnumerator<T> GetEnumerator()
+			=> list.GetEnumerator();
 
-			if (dictionary.Comparer.Equals(t, list.Last.Value))
-			{
-				return AddType.NO_CHANGE;
-			}
-			else if (dictionary.TryGetValue(t, out node))
-			{
-				list.Remove(node);
-				node = list.AddLast(t);
-				dictionary[t] = node;
-				return AddType.MOVED;
-			}
-			else
-			{
-				node = list.AddLast(t);
-				dictionary[t] = node;
-				return AddType.ADDED;
-			}
-
-		}
-
-		public bool Remove(T t)
-		{
-
-			LinkedListNode<T> node;
-
-			if (dictionary.TryGetValue(t, out node) && dictionary.Remove(t))
-			{
-				list.Remove(node);
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-
-		}
-
-		public void ExceptWith(IEnumerable<T> enumerable)
-		{
-			foreach (T t in enumerable)
-			{
-				Remove(t);
-			}
-		}
-
-		public IEnumerator<T> GetEnumerator ()
-		{
-			return list.GetEnumerator();
-		}
-
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator ()
-		{
-			return list.GetEnumerator();
-		}
-
-		public enum AddType
-		{
-
-			/// <summary>
-			/// No changes were made
-			/// </summary>
-			NO_CHANGE,
-
-			/// <summary>
-			/// The value was added
-			/// </summary>
-			ADDED,
-
-			/// <summary>
-			/// The value was moved to the end.
-			/// </summary>
-			MOVED
-
-		}
-
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		    => list.GetEnumerator();
 	}
 }
 
